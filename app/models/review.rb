5675f5625dd7ccbb4baa7029417d2a89aa235fd9
charlotte sucias shepherd <charlotte_sucias@hotmail.co.uk>
2024-10-09 12:28:51 +0100

    review model change

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,5 @@
 class Review < ApplicationRecord
   belongs_to :booking
   validates :comment, :rating, presence: true
-  validates :rating, numericality: { greater_than_or_equal_to: 0.0, less_than_or_equal_to: 5.0 }
+  validates :rating, numericality: { greater_than_or_equal_to: 1.0, less_than_or_equal_to: 5.0 }
 end
