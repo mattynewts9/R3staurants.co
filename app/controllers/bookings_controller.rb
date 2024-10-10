d4c40f5cf8befa0d6290815a5e4b9594ff400fc7
hamza khalid <kingkhanibf@gmail.com>
2024-10-10 15:28:49 +0100

    addingbookingbutton

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,17 +1,16 @@
 class BookingsController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :new ]
-  # before_action :set_restaurants
-  # before_action :set_booking, only: [:show, :edit, :update, :destroy]
+  skip_before_action :authenticate_user!, only: [:new]
+  before_action :set_restaurant, only: [:new, :create]
 
   def new
     @booking = Booking.new
-    @restaurant = Restaurant.find(params[:restaurant_id])
+    # The restaurant is already set by the before_action
   end
 
   def create
-    @booking = Booking.new(booking_params)
+    @booking = @restaurant.bookings.new(booking_params) # Assuming you have a relation set up
     if @booking.save
-      redirect_to booking_path(@booking)
+      redirect_to booking_path(@booking), notice: 'Booking created successfully.'
     else
       render :new, status: :unprocessable_entity
     end
@@ -20,7 +19,8 @@ class BookingsController < ApplicationController
   private
 
   def booking_params
-    params.require(:booking).permit(:date)
+    params.require(:booking).permit(:date) # Include any additional booking fields as needed
+  end
 
   def set_restaurant
     @restaurant = Restaurant.find(params[:restaurant_id])
