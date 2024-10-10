ffa97d99cfd71e7987f4a2a0e66ac7dda33dd567
charlotte sucias shepherd <charlotte_sucias@hotmail.co.uk>
2024-10-10 16:26:32 +0100

    tweaks

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,7 +1,8 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :restaurant
-  
+  has_many :reviews
+
   validates :start_date, :end_date, presence: true
   validate :end_date_after_start_date
   validates :start_date, presence: true # Ensure a booking date is provided
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,9 @@
 class Restaurant < ApplicationRecord
-  validates :name, :description, :image_url, presence: true
-  validates :image_url, uniqueness: true
-  validates :category, presence: true
   belongs_to :user
   has_many :bookings
   has_many :reviews, through: :bookings
+
+  validates :name, :description, :image_url, presence: true
+  validates :image_url, uniqueness: true
+  validates :category, presence: true
 end
