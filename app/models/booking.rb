5a9b995f2bcc21d612918ca6ad8569140390ab54
hamza khalid <kingkhanibf@gmail.com>
2024-10-10 12:02:31 +0100

    formforshowbooking

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,7 +2,16 @@ class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :restaurant
 
-  validates :start_date, presence: true  # Ensure a booking date is provided
-  validates :end_date, presence: true  # Ensure a booking time is provided
+  validates :start_date, :end_date, presence: true
+  validate :end_date_after_start_date
 
+  private
+
+  def end_date_after_start_date
+    return if end_date.blank? || start_date.blank?
+
+    if end_date < start_date
+      errors.add(:end_date, "must be after the start date")
+    end
+  end
 end
