acf8ddbe3c4456606d9a005fd9fe80e8a050537d
mathew newton <mattynewts@gmail.com>
2024-10-11 22:36:51 +0800

    linked about us

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   #  root to: "restaurants#index"
    devise_for :users
+   get 'about', to: 'pages#aboutus'
   # root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
